b7ed4c20c2b7daf8262c22dbf5eb87cae48a7469
Zach Dennis <zach.dennis@gmail.com>
2012-07-05 22:16:18 -0400

    Implementing the point namespace with struct point and functions for add, max, and min

--- clojure/drunken-cockroach/src/drunken_cockroach/point.clj
+++ clojure/drunken-cockroach/src/drunken_cockroach/point.clj
@@ -1,3 +1,38 @@
 (ns drunken-cockroach.point)
 
 (defstruct point :x :y)
+
+(defn add [point1 point2]
+  (let [
+    x1 (get point1 :x)
+    x2 (get point2 :x)
+    y1 (get point1 :y)
+    y2 (get point2 :y)]
+
+  (struct point
+    (+ x1 x2)
+    (+ y1 y2))))
+
+(defn max [point1 point2]
+  (let [
+    x1 (get point1 :x)
+    x2 (get point2 :x)
+    y1 (get point1 :y)
+    y2 (get point2 :y)]
+
+  (cond
+      (> y2 y1) point2
+      (> x2 x1) point2
+      :else point1)))
+
+(defn min [point1 point2]
+  (let [
+    x1 (get point1 :x)
+    x2 (get point2 :x)
+    y1 (get point1 :y)
+    y2 (get point2 :y)]
+
+  (cond
+      (< y2 y1) point2
+      (< x2 x1) point2
+      :else point1)))
